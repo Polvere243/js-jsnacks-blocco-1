6613a96d869851a10d1a1e585e13cbdfee3f321c
Polvere243 <danielercoli@gmail.com>
2023-12-26 22:19:30 +0100

    created a for cycle

--- array-lista/script.js
+++ array-lista/script.js
@@ -2,3 +2,17 @@ const fruits = ['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'c
 
 fruits.push("pesca");
 console.table(fruits);
+
+let isWatermelonIn = false; 
+
+for (let i = 0; i < fruits.length && !isWatermelonIn; i++) {
+    if ("cocomero" === fruits[i]){
+        console.log("Trovato! Devo solo preparare il cocktail.") 
+        isWatermelonIn = true;       
+    } else {
+        console.log("Oh no, devo uscire a comprare il cocomero!");
+    }
+       
+
+
+}
