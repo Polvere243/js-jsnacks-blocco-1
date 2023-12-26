b528e64fc78d98ba70539375cdc78a77e7ff4ae0
Polvere243 <danielercoli@gmail.com>
2023-12-26 22:23:04 +0100

    got element from the DOM

--- array-lista/script.js
+++ array-lista/script.js
@@ -1,4 +1,7 @@
 const fruits = ['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola'];
+const button = document.querySelector("button");
+const paragraph = document.querySelector("p");
+
 
 fruits.push("pesca");
 console.table(fruits);
