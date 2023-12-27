2079ce41790a08f6830c8a1b4f53585c1ebcf5e5
Polvere243 <danielercoli@gmail.com>
2023-12-27 01:18:33 +0100

    created two variables for the numbers I need to ask

--- js/array-tail.js
+++ js/array-tail.js
@@ -1,6 +1,10 @@
+// recupero gli elementi dal DOM
 const userNumbers = document.querySelector("input");
 const button = document.getElementById("number");
+const min = 1;
+const max = 100;
 
+// aggancio al bottone un ascoltatore di eventi per ottenere i numeri da aggiungere all'array
 button.addEventListener("click", function(){
-    
+    const numbers = Math.floor(Math.random)
 })
