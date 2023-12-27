ea9965024b5a7556d8b3f723dbedb1649ffd1914
Polvere243 <danielercoli@gmail.com>
2023-12-27 01:34:52 +0100

    maybe got what to do, but I will try tomorrow

--- js/array-tail.js
+++ js/array-tail.js
@@ -6,5 +6,7 @@ const max = 100;
 
 // aggancio al bottone un ascoltatore di eventi per ottenere i numeri da aggiungere all'array
 button.addEventListener("click", function(){
-    const numbers = Math.floor(Math.random)
+    const numbers = parseInt(userNumbers.value);
+    console.log(numbers);
+
 })
