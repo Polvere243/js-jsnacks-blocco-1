cc142e6b6aea31a7d49fe2574894d57484310860
Polvere243 <danielercoli@gmail.com>
2023-12-27 12:01:24 +0100

    about to use a prompt instead of the form to ask the number of elements

--- js/array-tail.js
+++ js/array-tail.js
@@ -1,21 +1,8 @@
-// recupero gli elementi dal DOM
-const userNumbers = document.querySelector("input");
-const button = document.getElementById("number");
+// genero le costanti che mi servono
 const min = 1;
 const max = 100;
 const chosenNumbers = [];
-// aggancio al bottone un ascoltatore di eventi per ottenere i numeri da aggiungere all'array
-button.addEventListener("click", function(){
-    const numbers = parseInt(userNumbers.value);
-    console.log(numbers);
-    let randomNumbers = 0;
-    for (let i = 0; i < numbers; i++){
-        randomNumbers = Math.floor(Math.random() * max) + 1;
-        console.log(randomNumbers);
-    }
-    chosenNumbers.push("randomNumbers");
-    console.table(chosenNumbers);
-})
+
 
 
 
