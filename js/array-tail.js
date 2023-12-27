f5cde216a1d050d4c53ba05eb31f7159a25d09c4
Polvere243 <danielercoli@gmail.com>
2023-12-27 11:29:56 +0100

    still trying to understand how to push random numbers into the array

--- js/array-tail.js
+++ js/array-tail.js
@@ -3,10 +3,19 @@ const userNumbers = document.querySelector("input");
 const button = document.getElementById("number");
 const min = 1;
 const max = 100;
-
+const chosenNumbers = [];
 // aggancio al bottone un ascoltatore di eventi per ottenere i numeri da aggiungere all'array
 button.addEventListener("click", function(){
     const numbers = parseInt(userNumbers.value);
     console.log(numbers);
-
+    let randomNumbers = 0;
+    for (let i = 0; i < numbers; i++){
+        randomNumbers = Math.floor(Math.random() * max) + 1;
+        console.log(randomNumbers);
+    }
+    chosenNumbers.push("randomNumbers");
+    console.table(chosenNumbers);
 })
+
+
+
