7fc430ccc51d48e9eaf1aa492b24a0a4accc564d
Polvere243 <danielercoli@gmail.com>
2023-12-27 17:21:54 +0100

    pushed random numbers into an array, not really confident about it

--- js/array-tail.js
+++ js/array-tail.js
@@ -1,8 +1,15 @@
 // genero le costanti che mi servono
 const min = 1;
 const max = 100;
-const chosenNumbers = [];
+let chosenNumbers = [];
 
+// chiedo all'utente quanti numeri vuole
+const numOfElements = parseInt(prompt("Quanti numeri vuoi nell'array?", "50"));
+console.log(numOfElements);
+// genero tanti numeri casuali quanti ne vuole l'utente
 
-
+for (let i = 0; i < numOfElements; i++){
+    chosenNumbers.push(Math.floor(Math.random() * (max + 1 - min)) + min);
+    console.table(chosenNumbers);
+}
 
